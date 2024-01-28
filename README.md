39f782d98829750698d61d79dc3f105e7cc21365
Henry Dollman <hank@henrygd.me>
2024-01-28 12:21:14 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,15 +1,224 @@
-# Elysia with Bun runtime
+# NCAA API
 
-## Getting Started
-To get started with this template, simply paste this command into your terminal:
-```bash
-bun create elysia ./elysia-example
+API to return consumable data from ncaa.com. Works with stats, rankings, standings, and history.
+
+## Usage
+
+Make a GET request using the same path as the URL on the main website. You'll get a JSON response with the data.
+
+You can test the API using the example host below. Host your own if you need it to be reliable long term.
+
+https://ncaa-api.henrygd.me/rankings/football/fbs/associated-press
+
+## Parameters
+
+The following URL parameters are supported:
+
+| Parameter | Description                 |
+| --------- | --------------------------- |
+| `page`    | Page number. Defaults to 1. |
+
+## Examples
+
+### Football - Associated Press rankings
+
+Website: https://www.ncaa.com/rankings/football/fbs/associated-press
+
+`GET /rankings/football/fbs/associated-press`
+
+```json
+{
+  "sport": "Football",
+  "title": "College football rankings: Associated Press Top 25",
+  "updated": "Through Games JAN. 8, 2024",
+  "page": 1,
+  "pages": 1,
+  "data": [
+    {
+      "RANK": "1",
+      "SCHOOL": "Michigan (61)",
+      "POINTS": "1525 ",
+      "PREVIOUS": "1",
+      "RECORD": "15-0"
+    },
+    {
+      "RANK": "2",
+      "SCHOOL": "Washington",
+      "POINTS": "1459",
+      "PREVIOUS": "2",
+      "RECORD": "14-1"
+    },
+    ...
+  ]
+}
+```
+
+### Football - scoring defense
+
+Website: https://www.ncaa.com/stats/football/fbs/current/team/28
+
+`GET /stats/football/fbs/current/team/28`
+
+```json
+{
+  "sport": "Football",
+  "title": "Scoring Defense",
+  "updated": "Monday, January 08, 2024 11:52 pm - Through games Monday, January 08, 2024",
+  "page": 1,
+  "pages": 3,
+  "data": [
+    {
+      "Rank": "1",
+      "Team": "Michigan",
+      "G": "15",
+      "TDs": "17",
+      "Opp XP": "15",
+      "2PT": "0",
+      "Opp DXP": "0",
+      "Opp FGM": "13",
+      "Opp Saf": "0",
+      "Pts": "156.00",
+      "Avg": "10.40"
+    },
+    {
+      "Rank": "2",
+      "Team": "Ohio St.",
+      "G": "13",
+      "TDs": "15",
+      "Opp XP": "14",
+      "2PT": "0",
+      "Opp DXP": "0",
+      "Opp FGM": "14",
+      "Opp Saf": "0",
+      "Pts": "146.00",
+      "Avg": "11.23"
+    },
+    ...
+  ]
+}
+```
+
+### Women's Basketball - ASUN conference rankings
+
+Website: https://www.ncaa.com/standings/basketball-women/d1/asun
+
+`GET /standings/basketball-women/d1/asun`
+
+```json
+{
+  "sport": "Women's Basketball",
+  "title": "ASUN CONFERENCE",
+  "updated": "Jan 26, 2024 05:03 AM EDT",
+  "page": 1,
+  "pages": 1,
+  "data": [
+    {
+      "School ": "FGCU",
+      "Conference W": "6",
+      "Conference L": "0",
+      "Conference PCT": "1.000",
+      "Overall W": "16",
+      "Overall L": "4",
+      "Overall PCT": "0.800",
+      "Overall STREAK": "Won 9"
+    },
+    {
+      "School ": "Central Ark.",
+      "Conference W": "5",
+      "Conference L": "1",
+      "Conference PCT": "0.833",
+      "Overall W": "13",
+      "Overall L": "6",
+      "Overall PCT": "0.684",
+      "Overall STREAK": "Won 2"
+    },
+    ...
+  ]
+}
+```
+
+### Women's Ice Hockey - Individual faceoff wins
+
+Website: https://www.ncaa.com/stats/icehockey-women/d1/current/individual/1261
+
+`GET /stats/icehockey-women/d1/current/individual/1261`
+
+```json
+{
+  "sport": "Women's Ice Hockey",
+  "title": "Faceoff wins",
+  "updated": "Wednesday, January 24, 2024 10:04 pm - Through games Wednesday, January 24, 2024",
+  "page": 1,
+  "pages": 6,
+  "data": [
+    {
+      "Rank": "1",
+      "Name": "Tessa Janecke",
+      "Team": "Penn St.",
+      "Cl": "So.",
+      "Games Plyd.": "26",
+      "FO won": "439",
+      "FO lost": "240"
+    },
+    {
+      "Rank": "2",
+      "Name": "Sadie Peart",
+      "Team": "Quinnipiac",
+      "Cl": "Sr.",
+      "Games Plyd.": "27",
+      "FO won": "372",
+      "FO lost": "290"
+    },
+    ...
+  ]
+}
+```
+
+### Bowling - NCAA championship history
+
+Website: https://www.ncaa.com/history/bowling/nc
+
+`GET /history/bowling/nc`
+
+```json
+{
+  "sport": "Bowling",
+  "title": "Championship History",
+  "updated": "",
+  "page": 1,
+  "pages": 1,
+  "data": [
+    {
+      "Year": "2023",
+      "Champion": "Vanderbilt",
+      "Coach": "John Williamson",
+      "Games": "3",
+      "Runner-Up": "Arkansas State",
+      "Host/Site": "Las Vegas, NV"
+    },
+    {
+      "Year": "2022",
+      "Champion": "McKendree",
+      "Coach": "Shannon O'Keefe",
+      "Games": "0",
+      "Runner-Up": "Stephen F. Austin",
+      "Host/Site": "Columbus, OH"
+    },
+    ...
+  ]
+}
 ```
 
 ## Development
-To start the development server run:
+
+This is an [Elysia](https://elysiajs.com/) app. To start the development server run:
+
 ```bash
 bun run dev
 ```
 
-Open http://localhost:3000/ with your browser to see the result.
+TODO - tests
+
+## Deployment
+
+TODO - dockerize
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,161 @@
-import { Elysia } from "elysia";
+import { Elysia, NotFoundError } from 'elysia'
+import { parseHTML } from 'linkedom'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const validPaths = ['stats', 'rankings', 'standings', 'history']
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// ELYSIA //////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+const app = new Elysia()
+	.get('/', ({ set }) => {
+		// redirect to github page
+		set.redirect = 'https://github.com/henrygd/ncaa-api'
+	})
+	.get('/*', async ({ query: { page }, path, set }) => {
+		set.headers['Content-Type'] = 'application/json'
+		// if production, cache for 30 min
+		if (process.env.NODE_ENV === 'production') {
+			set.headers['Cache-Control'] = 'public, max-age=1800'
+		}
+
+		// check that resource is valid
+		if (!validPaths.includes(path.split('/')[1])) {
+			set.status = 400
+			throw new Error('Invalid resource')
+		}
+
+		// check that page param is an int
+		if (page && !/^\d+$/.test(page)) {
+			set.status = 400
+			throw new Error('Page parameter must be an integer')
+		}
+
+		const data = await getData({ path, page })
+		return JSON.stringify(data)
+	})
+	.listen(3000)
+
+console.log(`Server is running at ${app.server?.hostname}:${app.server?.port}`)
+
+//////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// FUNCTIONS ////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Fetch data from ncaa.com
+ * @param opts.path - path to fetch from ncaa.com
+ * @param opts.page - page number to fetch
+ */
+async function getData(opts: { path: string; page?: string }) {
+	// fetch html
+	const res = await fetch(
+		`https://www.ncaa.com/${opts.path}${opts.page && Number(opts.page) > 1 ? `/p${opts.page}` : ''}`
+	)
+
+	if (!res.ok) {
+		throw new NotFoundError(JSON.stringify({ message: 'Resource not found' }))
+	}
+
+	// parse with linkedom
+	const { document } = parseHTML(await res.text())
+
+	const table = document.querySelector('main table') as HTMLTableElement
+
+	if (!table) {
+		throw new Error('Could not parse data')
+	}
+
+	// find general info
+	const sport = document.querySelector('h2.page-title')?.textContent?.trim() ?? ''
+
+	let title = ''
+	const titleEl = document.querySelectorAll(
+		'.stats-header__lower__title, main option[selected], main h1.node__title'
+	)?.[0]
+
+	if (titleEl) {
+		titleEl.querySelector('.hidden')?.remove()
+		title = titleEl.textContent?.trim() ?? ''
+	}
+
+	let updated =
+		document
+			.querySelectorAll(
+				'.stats-header__lower__desc, .rankings-last-updated, .standings-last-updated'
+			)?.[0]
+			?.textContent?.replace(/Last updated /i, '')
+			.trim() ?? ''
+
+	// figure out pages
+	let page = 1
+	let pages = 1
+	const tablePageLinks = document.querySelectorAll(
+		'ul.stats-pager li:not(.stats-pager__li--prev):not(.stats-pager__li--next)'
+	)
+	if (tablePageLinks.length > 0) {
+		page = [...tablePageLinks].findIndex((li) => li.classList.contains('active')) + 1
+		pages = tablePageLinks.length
+	}
+
+	const data = []
+
+	let headings: (string | null)[] = []
+
+	if (opts.path.includes('standings')) {
+		headings = getStandingsHeaders(table)
+	} else {
+		headings = [...table.querySelectorAll('thead th')].map((th) => th.textContent)
+	}
+
+	const rows = table.querySelectorAll('tbody tr:not(.subdiv-header)')
+	for (const row of rows) {
+		const rowData: Record<string, string> = {}
+		const cells = row.querySelectorAll('td')
+		for (let i = 0; i < cells.length; i++) {
+			const heading = headings[i]
+			if (heading) {
+				rowData[heading] = cells[i].textContent?.trim() ?? ''
+			}
+		}
+		data.push(rowData)
+	}
+
+	return { sport, title, updated, page, pages, data }
+}
+
+/**
+ * Merge table headers that span multiple column.
+ * Use the first row of headers as the base according to colspan and
+ * concat the second row th textContent to the first row th textContent
+ * @param table - table element to parse
+ */
+function getStandingsHeaders(table: HTMLTableElement) {
+	const tableRowOne = table.querySelector('.standings-table-header')
+	const tableRowTwo = table.querySelector('.standings-table-subheader')
+
+	if (!tableRowOne || !tableRowTwo) {
+		throw new Error('Could not parse data')
+	}
+
+	const headings: (string | null)[] = []
+	const rowOneHeadings: string[] = []
+
+	const rowOneThs = tableRowOne.querySelectorAll('th')
+	for (const th of rowOneThs) {
+		const colspan = Number(th.getAttribute('colspan')) || 1
+		const heading = th.textContent?.trim() ?? ''
+		for (let i = 0; i < colspan; i++) {
+			rowOneHeadings.push(heading)
+		}
+	}
+
+	const rowTwoThs = tableRowTwo.querySelectorAll('th')
+	for (let i = 0; i < rowTwoThs.length; i++) {
+		const th = rowTwoThs[i]
+		const heading = th.textContent?.trim() ?? ''
+		headings.push(rowOneHeadings[i] + ' ' + heading)
+	}
+
+	return headings
+}
